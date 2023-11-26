8e9371f3840ad406ad35aaccf7650789c35b4079
Vilayat <vilayatk96@outlook.com>
2023-11-27 00:19:01 +0530

    Add Team and Employee classes

---
+++ ParkingBackend/Models/Employee.cs
@@ -0,0 +1,12 @@
+﻿using System.Text.Json.Serialization;
+
+namespace ParkingBackend.Models
+{
+    public class Employee
+    {
+        public string Name { get; set; } = string.Empty;
+
+        [JsonIgnore]
+        public Team Team { get; set; }
+    }
+}
---
+++ ParkingBackend/Models/Team.cs
@@ -0,0 +1,10 @@
+﻿namespace ParkingBackend.Models
+{
+    public class Team
+    {
+        public string Name { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+        
+        List<Employee> TeamMembers { get; set; } 
+    }
+}

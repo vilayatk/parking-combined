6acfd3959fc677281b013f4d0567f5e2d78295b6
Vilayat <vilayatk96@outlook.com>
2023-11-26 22:18:53 +0530

    Remove template controller and model

--- ParkingBackend/Controllers/ParkingSpacesController.cs
+++ ParkingBackend/Controllers/ParkingSpacesController.cs
@@ -8,11 +8,12 @@ namespace ParkingBackend.Controllers
     public class ParkingSpacesController : ControllerBase
     {
         /*
-            1. Route("/products) => www.localhost:3000/products
-            2. Route("/products/{id} => www.localhost:3000/products/2 => Controller action parameter taken from URL, 
+         * Routing is case insensitive
+            1.  Route("/products) => www.localhost:3000/products
+            2.  Route("/products/{id} => www.localhost:3000/products/2 => Controller action parameter taken from URL, 
                 2 is passed as a parameter for the function
-            3. Route("/products/{id?}) => Means that id is optional action parameter
-            4. Route("[controller]") => url directs to controller name; ClassName - Controller = [controller]
+            3.  Route("/products/{id?}) => Means that id is optional action parameter
+            4.  Route("[controller]") => url directs to controller name; ClassName - Controller = [controller]
         */
 
 
